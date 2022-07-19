f635218af72afdd3f87fdb0d08f14c3af03ad55a
Дмитрий Никифоров <nikiforov_d.1973@mail.ru>
2022-07-19 18:39:09 +0400

    decorator_new

--- decorator.py
+++ decorator.py
@@ -20,10 +20,44 @@ def decor_fun(func):
     return time_date_func
 
 
+# @decor_fun
+# def sum_test_decor(a: int, b: int) -> int:
+#     return a * b
+#
+#
+# sum_test_decor(6, 7)
+class MeIterator:
+
+    def __init__(self, list_):
+        self._list = list_
+        self.cursor = 0
+        self.nest_cursor = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.cursor < len(self._list):
+            if self.nest_cursor < len(self._list[self.cursor]):
+                lst = self._list[self.cursor][self.nest_cursor]
+                self.nest_cursor += 1
+                return lst
+            self.cursor += 1
+            self.nest_cursor = 0
+        raise StopIteration
+
 @decor_fun
-def sum_test_decor(a: int, b: int) -> int:
-    return a * b
+def main():
+    nested_list = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None],
+    ]
+    for item in MeIterator(nested_list):
+        print(item)
+    print('----------')
+    flat_list = [item for item in MeIterator(nested_list)]
+    print(flat_list)
 
 
-sum_test_decor(6, 7)
-
+main()
