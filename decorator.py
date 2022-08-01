2fd4c650a79ae10a4ba26717f6855cd2b59115dd
Дмитрий Никифоров <nikiforov_d.1973@mail.ru>
2022-08-01 14:11:39 +0400

    corrected

--- decorator.py
+++ decorator.py
@@ -3,6 +3,25 @@ from datetime import datetime
 import os
 
 
+def decor_fun(func):
+    CACHE_TIME = {}
+
+    def time_date_func(*args, **kwargs):
+        tm = str(datetime.now().time())
+        dt = str(datetime.now().date())
+        CACHE_TIME["date"] = dt
+        CACHE_TIME["time"] = tm
+        CACHE_TIME["name function"] = func.__name__
+        CACHE_TIME["log"] = os.getcwd()
+        CACHE_TIME["start"] = func(*args, **kwargs)
+        CACHE_TIME["stop"] = func(*args, **kwargs)
+        new_func = func(*args, **kwargs)
+        with open('file_log', 'w') as f:
+            json.dump(CACHE_TIME, f)
+        return new_func
+    return time_date_func
+
+
 def pathway(file_name):
     def decor_fun(func):
         CACHE_TIME = {}
@@ -16,11 +35,15 @@ def pathway(file_name):
             CACHE_TIME["log"] = os.getcwd()
             CACHE_TIME["start"] = func(*args, **kwargs)
             CACHE_TIME["stop"] = func(*args, **kwargs)
+            new_func = func(*args, **kwargs)
             with open(file_name, 'w') as f:
                 json.dump(CACHE_TIME, f)
+            return new_func
 
         return time_date_func
+
     return decor_fun
+
 
 class MeIterator:
 
@@ -56,5 +79,16 @@ def main():
     flat_list = [item for item in MeIterator(nested_list)]
     print(flat_list)
 
+
+@decor_fun
+def summator(x, y):
+    return x + y
+
+
+three = summator(1, 2)
+five = summator(2, 3)
+result = summator(three, five)
+print('result: ', result)
+print('result type: ', type(result))
 
 main()
